e729999d75b99da4b9b5be53acc28d812ef7a520
Noy <noy@orbb.com>
2026-01-16 08:47:03 -0500

    feat: add content barrel export

---
+++ content/index.ts
@@ -0,0 +1,18 @@
+export { siteConfig } from "./site";
+export { homepage } from "./homepage";
+export { team, founders, advisors, coordinators } from "./team";
+export type { TeamMember } from "./team";
+export { stories, getStoryBySlug, getRecentStories } from "./stories";
+export type { Story } from "./stories";
+export {
+  articles,
+  categoryLabels,
+  getArticleBySlug,
+  getArticlesByCategory,
+  getRecentArticles,
+} from "./articles";
+export type { Article, ArticleCategory } from "./articles";
+export { mediaCoverage } from "./media";
+export type { MediaCoverage } from "./media";
+export { phoneGuide } from "./phone-guide";
+export { legal } from "./legal";
