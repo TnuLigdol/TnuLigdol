00f2f51a1a49a565a91ecaaf984ea17cd20c83e4
Noy <noy@orbb.com>
2026-01-16 10:54:21 -0500

    feat: add home components barrel export

---
+++ components/home/index.ts
@@ -0,0 +1,5 @@
+export { Hero } from "./hero";
+export { About } from "./about";
+export { FAQ } from "./faq";
+export { StoriesSection } from "./stories-section";
+export { PhoneGuideSection } from "./phone-guide-section";
